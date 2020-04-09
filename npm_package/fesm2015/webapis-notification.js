ada68e07a44d4e42a83f6d246b91cafbce52ac65
JiaLiPassion <JiaLi.Passion@gmail.com>
2020-04-09 20:54:56 +0900

    commit with new zone.js npm package release contents

---
+++ npm_package/fesm2015/webapis-notification.js
@@ -0,0 +1,30 @@
+/**
+* @license Angular v<unknown>
+* (c) 2010-2020 Google LLC. https://angular.io/
+* License: MIT
+*/
+(function (factory) {
+    typeof define === 'function' && define.amd ? define(factory) :
+    factory();
+}((function () { 'use strict';
+
+    /**
+     * @license
+     * Copyright Google Inc. All Rights Reserved.
+     *
+     * Use of this source code is governed by an MIT-style license that can be
+     * found in the LICENSE file at https://angular.io/license
+     */
+    Zone.__load_patch('notification', (global, Zone, api) => {
+        const Notification = global['Notification'];
+        if (!Notification || !Notification.prototype) {
+            return;
+        }
+        const desc = Object.getOwnPropertyDescriptor(Notification.prototype, 'onerror');
+        if (!desc || !desc.configurable) {
+            return;
+        }
+        api.patchOnProperties(Notification.prototype, null);
+    });
+
+})));
